f8f53e5a1906c482ce2b46b4a54966516dc7aba2
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-17 23:40:28 +0900

    revise

--- lib/configure.sh
+++ lib/configure.sh
@@ -9,7 +9,7 @@ function configure {
     if [ -e $HOME/.setuptools/config/master ]; then
         rm -f $HOME/.setuptools/config/master
     fi
-    $HOME/.setuptools/config/master
+    touch $HOME/.setuptools/config/master
 
     if [ ${PLATFORM} = "macOS" ]; then
         echo "PLATFORM=macOS" >> $HOME/.setuptools/config/master
