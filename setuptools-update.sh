e743e63f7e8cd4e1a886f3b0242d513e15a50bde
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-17 15:56:33 +0900

    update updator

--- setuptools-update.sh
+++ setuptools-update.sh
@@ -7,9 +7,12 @@ cd $HOME
 mkdir .setuptools_update_tmp
 cd .setuptools_update_tmp
 
-git clone https://github.com/shotastage/setuptools.sh.git
+echo "⬇️  Downloading newest package..."
+git clone --quiet https://github.com/shotastage/setuptools.sh.git >> /dev/null
 cd setuptools.sh
 
+
+echo "🔄  Updating setuptools..."
 cp -rf ./lib/ $HOME/.setuptools/lib/
 cp -f setuptools.sh $HOME/.setuptools/bin/setuptools
 cp -f setuptools.sh $HOME/.setuptools/bin/uptool
