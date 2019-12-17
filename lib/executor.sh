aee3c7aad386d69d1125956caba4827715207b83
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-17 11:53:43 +0900

    update executor

--- lib/executor.sh
+++ lib/executor.sh
@@ -2,16 +2,15 @@
 set -e
 
 
-
 WORKING_DIRECTORY=~/.setuptools
 MASTER_URL="https://raw.githubusercontent.com/shotastage/setuptools.sh/master"
 
 
 function execute {
     cd $WORKING_DIRECTORY/strategies/
-    curl --silent -O "${MASTER_URL}/mac-${1}.sh"
-    chmod +x mac-${1}.sh
-    . mac-${1}.sh
+    curl --silent -O "${MASTER_URL}/mac-${2}.sh"
+    chmod +x mac-${2}.sh
+    . mac-${2}.sh
 
     if [ ${1} = "install" ]; then
         install
