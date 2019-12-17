b0bffb7df7cf5871aa8b6a91e9715027f9ff5648
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-17 12:07:42 +0900

    revise

--- lib/workspace.sh
+++ lib/workspace.sh
@@ -6,24 +6,23 @@ function initialize {
     if [ -e $WORKING_DIRECTORY ]; then
         echo "Date: $(date)    User: $(whoami)" >> $WORKING_DIRECTORY/exec_user_log
     else
-        echo "Initializing..."
-        mkdir $WORKING_DIRECTORY
-        mkdir -p $WORKING_DIRECTORY/strategies/
-        mkdir -p $WORKING_DIRECTORY/toolchains/
-        mkdir -p $WORKING_DIRECTORY/bin/
+        make_directories
     fi
 }
 
 
 function prepare_workspace {
-    if [ -e $WORKING_DIRECTORY ]; then
-        return
+    if [! -e $WORKING_DIRECTORY ]; then
+        make_directories
     fi
 
-    if [ -e $WORKING_DIRECTORY/strategies/ ]; then
-        return
+    if [! -e $WORKING_DIRECTORY/strategies/ ]; then
+        make_directories
     fi
+}
 
+
+function make_directories {
     echo "Initializing..."
     mkdir $WORKING_DIRECTORY
     mkdir -p $WORKING_DIRECTORY/strategies/
