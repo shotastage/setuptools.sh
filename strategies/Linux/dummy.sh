686f401718525105fbeb61e29439a626f790d35d
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-17 23:20:48 +0900

    revise

---
+++ strategies/Linux/dummy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+STRATEGY_VERSION="v001"
+
+function install {
+    echo "THIS IS INSTALL SECTION..."
+}
+
+function uninstall {
+    echo "THIS IS UNINSTALL SECTION..."
+}
+
+function update {
+    echo "THIS IS UPDATE SECTION..."
+}
+
+function main {
+    install
+}
