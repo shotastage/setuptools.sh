8cb89dba9ccee1c624abb57899da18d2521798b8
shotan <hornet.live.mf@gmail.com>
2019-12-18 18:05:13 +0900

    add new str

---
+++ strategies/Linux/telegram.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+STRATEGY_VERSION="v001"
+
+
+function install {
+    echo "⬇️  Installing application..."
+    sudo snap install telegram-desktop
+}
+
+function uninstall {
+    echo "NOW UNDER CONSTRUCTION."
+}
+
+function update {
+    echo "NOW UNDER CONSTRUCTION."
+}
+
+function main {
+    install
+}
