83cf4ebc2716bc546d98b7f6bd25616c5c449ee0
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-18 17:25:04 +0900

    update seach

--- lib/strategies.sh
+++ lib/strategies.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 
+. $HOME/.setuptools/lib/platform.sh
+
 function search_strategy {
     echo "🔎  Searching..."
-    res=`curl -LI https://raw.githubusercontent.com/shotastage/setuptools.sh/master/mac-${1}.sh -w '%{http_code}\n' -s -o /dev/null`
+    res=`curl -LI https://raw.githubusercontent.com/shotastage/setuptools.sh/master/strategies/$(operating_system)/${1}.sh -w '%{http_code}\n' -s -o /dev/null`
     if [ 404 = "${res}" ]; then
         echo "Result: Strategy $1 does not exists!" 
     else
