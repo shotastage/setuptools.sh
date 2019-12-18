2507e038733cec547d726c0030380b924cc98ea1
shotan <hornet.live.mf@gmail.com>
2019-12-18 15:13:46 +0900

    revis

--- strategies/Linux/slack.sh
+++ strategies/Linux/slack.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
-
 STRATEGY_VERSION="v001"
 
 
 function install {
-    sudo snap install slack --classic
+    echo "⬇️  Downloading application..."
+    curl -OL "https://downloads.slack-edge.com/linux_releases/slack-desktop-4.2.0-amd64.deb"
+    if [ -e slack-desktop-4.2.0-amd64.deb ]; then
+        echo "⬇️  Installing application..."
+        sudo dpkg -i slack-desktop-4.2.0-amd64.deb
+    else
+        echo "⚠️ パッケージのダウンロードに失敗しました！"
+        echo "スクリプトを終了します [ Press Return to continue]"
+        read
+        exit 1
+    fi
 }
 
 function uninstall {
