cb4e51b6361cc61c5570986208f29d6a25770454
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-18 16:58:10 +0900

    add new str

---
+++ strategies/Linux/spotify.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+STRATEGY_VERSION="v001"
+
+
+function install {
+    echo "⬇️  Installing application..."
+    sudo snap install spotify
+}
+
+function uninstall {
+    echo "NOW UNDER CONSTRUCTION."
+}
+
+function update {
+    echo "NOW UNDER CONSTRUCTION."
+}
+
+function main {
+    install
+}
