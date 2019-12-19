af639e76ef6fae6cb76d35eb6dbfb722d3be06b1
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-19 23:42:37 +0900

    update uninstall script

--- tools/uninstall.sh
+++ tools/uninstall.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
+set -e
+
+if [ ${1} = "--purge" ]; then
+    rm -rf .setuptools/
+    echo "🆗  Completely uninstalled setuptools."
+    exit 0
+fi
 
 if [ -e $HOME/.setuptools/ ]; then
+    cd $HOME/.setuptools/
+    mv toolchains $HOME/.uptool_toolchains_backup/
     cd
     rm -rf .setuptools/
-    echo "Completely uninstalled setuptools."
     exit 0
 fi
 
