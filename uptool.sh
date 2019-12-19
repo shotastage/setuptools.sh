4dfcba35842f2f9571eedd80eb5dcd6e46148aff
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-19 23:56:31 +0900

    update cmd parser

--- uptool.sh
+++ uptool.sh
@@ -17,65 +17,62 @@ cd $WORKING_DIRECTORY
 . $WORKING_DIRECTORY/lib/strategies.sh
 . $WORKING_DIRECTORY/lib/configure.sh
 
+
+
+
 splash 
 initialize
 prepare_workspace
 
 
 
-if [ ${1} = "install" ] || [ ${1} = "i" ]; then
-    execute install $2
-    exit 0
-fi
+for OPT in "$@"
+do
+    case $OPT in
+        i | install )
+            execute install $2
+            exit 0
+            ;;
+        u | update )
+            if [ ${2} = "self" ]; then
+                setuptools-update
+                exit 0
+            fi
 
-
-if [ ${1} = "uninstall" ]; then
-
-    execute uninstall $2
-    exit 0
-fi
-
-if [ ${1} = "update" ] || [ ${1} = "u" ]; then
-    if [ ${2} = "self" ]; then
-        setuptools-update
-        exit 0
-    fi
-
-    execute update $2
-    exit 0
-fi
-
-if [ ${1} = "search" ] || [ ${1} = "s" ]; then
-    search_strategy $2
-    exit 0
-fi
-
-if [ ${1} = "configure" ]; then
-    echo "🔄  Generating configuration..."
-    configure
-    exit 0
-fi
-
-if [ ${1} = "help" ] || [ ${1} = "h" ]; then
-    setuptools-help
-    exit 0
-fi
-
-if [ ${1} = "version" ] || [ ${1} = "v" ]; then
-    echo
-    echo "setuptools 1.1.0"
-    echo "Copyright (C) 2019 Shota Shimazu, All rights reserved."
-    echo 
-    exit 0
-fi
-
-if [ ${1} = "show-user-log" ]; then
-    cat $WORKING_DIRECTORY/exec_user_log
-    exit 0
-fi
-
-if [ ${1} = "--clear-strategies" ]; then
-    cd strategies
-    rm -rf *
-    exit 0
-fi
+            execute update $2
+            exit 0
+            ;;
+        s | search )
+            search_strategy $2
+            exit 0
+            ;;
+        uninstall )
+            execute uninstall $2
+            exit 0
+            ;;
+        configure )
+            echo "🔄  Generating configuration..."
+            configure
+            exit 0
+            ;;
+        h | help )
+            setuptools-help
+            exit 0
+            ;;
+        v | version )
+            echo
+            echo "setuptools 1.1.0"
+            echo "Copyright (C) 2019 Shota Shimazu, All rights reserved."
+            echo 
+            exit 0
+            ;;
+        --show-user-log )
+            cat $WORKING_DIRECTORY/exec_user_log
+            exit 0
+            ;;
+        --clear-strategies )
+            cat $WORKING_DIRECTORY/exec_user_log
+            exit 0
+            ;;
+    esac
+done
