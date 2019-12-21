1656d705634561a457f3de86e545fb25ded44feb
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-21 15:55:49 +0900

    add new telegram str

--- strategies/macOS/telegram.sh
+++ strategies/macOS/telegram.sh
@@ -1,21 +1,49 @@
 #!/usr/bin/env bash
 
 STRATEGY_VERSION="v001"
-CURRENT_ID="747648890"
 
 
 function install {
-    brew update
-    brew install mas
-    mas install $CURRENT_ID
+    if [ -e /Applications/Telegram.app ]; then
+        echo "🖥  Telegramはすでにインストールされています."
+        return
+    fi
+    echo "⬇️  Telegramをインストールします."
+    echo "⬇️  ファイルをダウンロードしています..."
+    curl -OL "https://osx.telegram.org/updates/TelegramMac.dmg"
+    if [ -e TelegramMac.dmg ]; then
+        echo "💿  イメージをマウントしています..."
+        hdiutil mount TelegramMac.dmg >> /dev/null
+        echo "⬇️  アプリケーションをインストールしています..."
+        cp -r /Volumes/Telegram/Telegram.app /Applications/
+    else
+        echo "⚠️ パッケージのダウンロードに失敗しました！"
+        echo "スクリプトを終了します [ Press Return to continue]"
+        read
+        exit 1
+    fi
+
+    echo "💿  イメージをアンマウントしています..."
+    umount /Volumes/Telegram/ >> /dev/null
+    echo "🧹  クリーンアップしています..."
+    rm TelegramMac.dmg
 }
+
 
 function uninstall {
-    sudo mas uninstall $CURRENT_ID
+    cd /Applications/
+    echo "🧹 Removing application..."
+    rm -rf Firefox.app
+    echo "🧹 Removing remaining files..."
+    rm -rf ~/Library/Application\ Support/Firefox/
+    rm -rf ~/Library/Caches/Firefox/
+    rm -f ~/Library/Preferences/org.mozilla.firefox.plist
+    echo "🧹 Firefox has been uninstalled!"
 }
 
+
 function update {
-    mas upgrade $CURRENT_ID
+    echo "NOW UNDER CONSTRUCTION."
 }
 
 function main {
