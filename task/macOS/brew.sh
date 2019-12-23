03e35081779e93c2e08ee305d34a699ef1afb9ac
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-23 22:53:32 +0900

    add initial script

---
+++ task/macOS/brew.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+function uptask() {
+    brew update
+    brew upgrade
+}
---
+++ task/macOS/create-upfile.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+function uptask() {
+    if [ -e ./Upfile ]; then
+        echo "📄  Upfile already exists!"
+        exit 1
+    fi
+
+    echo "📄 Creating upfile..."
+    touch Upfile
+
+    echo "🖊  Writing contents..."
+    echo "########## Upfile ##########" >> ./Upfile
+    echo "Strategies="
+}
