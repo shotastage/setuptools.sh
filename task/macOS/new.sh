8bb845c9fba031f889f459012c23bc7ec285e3de
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-26 15:03:01 +0900

    revise

--- task/macOS/new.sh
+++ task/macOS/new.sh
@@ -4,10 +4,13 @@
 function uptask() {
     echo "Project name:"
     read pjname
+    echo "Description:"
+    read pjdesc
     mkdir $pjname
     cd $pjname
     git init
     echo "${pjname}" >> README.md
     echo "==========" >> README.md
-    echo "Project description is here"
+    echo $pjdesc >> README.md
+    uptool t create-upfile
 }
