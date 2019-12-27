f0c6da7bfd599e0e3fa50fa6804afe960b2f4da4
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-27 16:46:01 +0900

    revise

--- lib/codesign.sh
+++ lib/codesign.sh
@@ -8,11 +8,7 @@ set -e
 verify_script() {
     echo "🔑  Verifying script..."
 
-
-    echo $1
-    echo $2
-
-    curl --quiet -O $2 >> /dev/null
+    curl --silent -O $2 >> /dev/null
 
     if [ ! "`gpg --verify $1.sig $1 | grep 'Good signature'`" ]; then
         echo "❌  Bad signature! "
