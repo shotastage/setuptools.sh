de336a3409724f6c3bdfbda84b8a3dfbf3da5b16
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-27 13:14:54 +0900

    revise

--- lib/codesign.sh
+++ lib/codesign.sh
@@ -4,6 +4,6 @@
 sign_master() {
     for fname in ./strategies/macOS/*.sh; do
         echo "🔑  Signinig ${fname} ..."
-        gpg -u 86133173EF57B60825297D8C374773E889C0E769 -b $fname
+        gpg --batch --yes -u 86133173EF57B60825297D8C374773E889C0E769 -b $fname
     done
 }
