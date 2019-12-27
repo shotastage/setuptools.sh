bcebe10c76cb2711b16995ab610246d1a8bd8daa
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-27 17:19:08 +0900

    revise

--- lib/codesign.sh
+++ lib/codesign.sh
@@ -5,6 +5,8 @@ verify_script() {
     echo "🔑  Verifying script..."
 
     curl --silent -O $2 >> /dev/null
+
+    echo "sddqwoqwijdiqwdjoqwdiqwojdiqwoa"
        
     LANG=C LC_MESSAGES=C gpg --verify $1.sig $1 2>&1 | grep WARNING > /dev/null
     if [[ "${PIPESTATUS[0]} ${PIPESTATUS[1]}" == "0 1" ]]; then
