7da7e838c14452f1c5148130dbf7cfc2e76bab0a
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-27 14:23:14 +0900

    add new

---
+++ task/macOS/vscode-latex.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+uptask() {
+    if [ -e ./.vscode/settings.json ]; then
+        mv ./.vscode/settings.json ./.vscode/settings-backup.json
+    fi
+
+    echo CONTENT >> ./.vscode/settings.json
+}
+
+
+CONTENT=$(cat << EOS
+{
+    "latex-workshop.latex.tools": [
+        {
+          "command": "ptex2pdf",
+          "args": ["-l", "-ot", "-kanji=utf8 -synctex=1", "%DOC%"],
+          "name": "Step 1: ptex2pdf"
+        },
+        {
+          "command": "pbibtex",
+          "args": ["%DOCFILE%", "-kanji=utf8"],
+          "name": "Step 2: pbibtex"
+        },
+        {
+          "command": "ptex2pdf",
+          "args": ["-l", "-ot", "-kanji=utf8 -synctex=1", "%DOC%"],
+          "name": "Step 3: ptex2pdf"
+        },
+        {
+          "command": "ptex2pdf",
+          "args": ["-l", "-ot", "-kanji=utf8 -synctex=1", "%DOC%"],
+          "name": "Step 4: ptex2pdf"
+        }
+      ],
+      "latex-workshop.latex.recipes": [
+        {
+          "name": "toolchain",
+          "tools": [
+            "Step 1: ptex2pdf",
+            "Step 2: pbibtex",
+            "Step 3: ptex2pdf",
+            "Step 4: ptex2pdf"
+          ]
+        }
+      ],
+      "latex-workshop.view.pdf.viewer": "tab",
+      "latex-workshop.chktex.enabled": false
+}
+EOS
+)
+
