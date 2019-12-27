b93913a7b397b26287f19438422eb440efb8057b
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-27 13:39:50 +0900

    revise

--- lib/codesign.sh
+++ lib/codesign.sh
@@ -5,6 +5,14 @@ sign_master() {
     for fname in ./strategies/**/*.sh; do
         echo "🔑  Signinig strategy ${fname} ..."
         gpg --batch --yes -u 86133173EF57B60825297D8C374773E889C0E769 -b $fname
+
+        if [ `$fname | grep macOS` ]; then
+            mv $fname ./strategies/macOS/signatures/
+        fi
+
+        if [ `$fname | grep Linux` ]; then
+            mv $fname ./strategies/Linux/signatures/
+        fi
     done
 
     for fname in ./task/**/*.sh; do
