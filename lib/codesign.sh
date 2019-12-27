e105ecf92104de2f5bc0243d1e80bce6bcbb6dc3
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-27 13:17:25 +0900

    update codesign tool

--- lib/codesign.sh
+++ lib/codesign.sh
@@ -3,7 +3,12 @@
 
 sign_master() {
     for fname in ./strategies/**/*.sh; do
-        echo "🔑  Signinig ${fname} ..."
+        echo "🔑  Signinig strategy ${fname} ..."
+        gpg --batch --yes -u 86133173EF57B60825297D8C374773E889C0E769 -b $fname
+    done
+
+    for fname in ./taks/**/*.sh; do
+        echo "🔑  Signinig task ${fname} ..."
         gpg --batch --yes -u 86133173EF57B60825297D8C374773E889C0E769 -b $fname
     done
 }
