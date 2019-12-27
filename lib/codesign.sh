4db60bb85054de900323d06cbac6faf82618a0ae
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-27 17:54:25 +0900

    revise

--- lib/codesign.sh
+++ lib/codesign.sh
@@ -6,7 +6,7 @@ verify_script() {
 
     curl --silent -O $2 >> /dev/null
        
-    gpg --quiet --verify $1.sig $1 >> /dev/null
+    gpg --quiet --verify $1.sig $1 2>/dev/null
 
     if [ $? -eq 0 ]; then
         echo "🔑  Script is valid."
