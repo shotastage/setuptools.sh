2473a2278e34bca9f35280a1510f1a0cbdc74ddd
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-27 15:11:56 +0900

    revise

--- lib/masterdev.sh
+++ lib/masterdev.sh
@@ -11,29 +11,11 @@ sign_master() {
     for fname in ./strategies/**/*.sh; do
         echo "🔑  Signinig strategy ${fname} ..."
         gpg --batch --yes -u 86133173EF57B60825297D8C374773E889C0E769 -b $fname
-
-        if [ `$fname | grep macOS` ]; then
-
-            echo $fname.sig
-            mv $fname.sig ./strategies/macOS/signatures/
-        fi
-
-        if [ `$fname | grep Linux` ]; then
-            mv $fname.sig ./strategies/Linux/signatures/
-        fi
     done
 
     for fname in ./task/**/*.sh; do
         echo "🔑  Signinig task ${fname} ..."
         gpg --batch --yes -u 86133173EF57B60825297D8C374773E889C0E769 -b $fname
-
-        if [ `$fname | grep macOS` ]; then
-            mv $fname.sig ./task/macOS/signatures/
-        fi
-
-        if [ `$fname | grep Linux` ]; then
-            mv $fname.sig ./task/Linux/signatures/
-        fi
     done
 }
 
