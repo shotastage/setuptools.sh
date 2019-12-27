4ecb4a2cc626795318d8e0770d6f67e526631466
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-27 13:19:41 +0900

    revise

--- lib/codesign.sh
+++ lib/codesign.sh
@@ -7,7 +7,7 @@ sign_master() {
         gpg --batch --yes -u 86133173EF57B60825297D8C374773E889C0E769 -b $fname
     done
 
-    for fname in ./tasks/**/*.sh; do
+    for fname in ./task/**/*.sh; do
         echo "🔑  Signinig task ${fname} ..."
         gpg --batch --yes -u 86133173EF57B60825297D8C374773E889C0E769 -b $fname
     done
