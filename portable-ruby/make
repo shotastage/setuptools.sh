18ce5e57739dbe6baf176e2045450e5ccd72727d
Shota Shimazu <hornet.live.mf@gmail.com>
2019-12-28 19:13:08 +0900

    revise

---
+++ portable-ruby/make
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "Now under construction."
---
+++ strategies/macOS/github.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+STRATEGY_VERSION="v001"
+
+. ~/.setuptools/lib/foundation.sh
+
+function install {
+    if [ -e /Applications/Github\ Desktop.app/ ]; then
+        echo "🖥  Githubはすでにインストールされています."
+        return
+    fi
+    echo "⬇️  Githubをインストールします."
+    echo "⬇️  ファイルをダウンロードしています..."
+    curl -OL "https://desktop.githubusercontent.com/releases/2.2.4-be951a19/GitHubDesktop.zip"
+    if [ -e GitHubDesktop.zip ]; then
+        echo "💿  イメージをマウントしています..."
+        unzip GitHubDesktop.zip >> /dev/null
+        echo "⬇️  アプリケーションをインストールしています..."
+        cp -r Github\ Desktop.app /Applications/
+    else
+        echo "⚠️ パッケージのダウンロードに失敗しました！"
+        echo "スクリプトを終了します [ Press Return to continue]"
+        read
+        exit 1
+    fi
+
+    echo "💿  イメージをアンマウントしています..."
+    defer_dmg "Google Chrome"
+    echo "🧹  クリーンアップしています..."
+    rm googlechrome.dmg
+}
+
+function uninstall {
+    cd /Applications/
+    rm -rf Google\ Chrome.app
+}
+
+function update {
+    echo "NOW UNDER CONSTRUCTION!"
+}
+
+function main {
+    install
+}
