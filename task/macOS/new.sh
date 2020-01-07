333863b7e46d36e143d86f445713340e0287b963
Shota Shimazu <hornet.live.mf@gmail.com>
2020-01-07 12:10:31 +0900

    update new task

--- task/macOS/new.sh
+++ task/macOS/new.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-function uptask() {
+uptask() {
     echo "Project name:"
     read pjname
     echo "Description:"
@@ -13,4 +13,37 @@ function uptask() {
     echo "==========" >> README.md
     echo $pjdesc >> README.md
     uptool t create-upfile
+}
+
+prj_template() {
+    echo "Please choose project template."
+}
+
+
+front_menu() {
+    echo "[1] Server-side or Backend Services"
+    echo "[2] Web front-end program"
+    echo "[3] Desktop App"
+    echo "[4] Mobile or Smartphone Application"
+    echo "[5] Others"
+}
+
+server_menu() {
+    echo "[1] Go Backend Service"
+    echo "[2] Go Backend Service + Microservice"
+    echo "[3] Django Project"
+    echo "[4] Ruby on Rails Project"
+}
+
+web_menu() {
+    echo "[1] React.js Project"
+    echo "[2] Vue.js Project"
+    echo "[3] Angular Project"
+    echo "[4] WordPress Theme"
+}
+
+desktop_menu() {
+    echo "[1] Electron App"
+    echo "[2] macOS Application"
+    echo "[3] Win32 Application"
 }
