d7586a05ba77e05961878044442e0c5bade0ae48
Shota Shimazu <hornet.live.mf@gmail.com>
2020-01-09 13:43:48 +0900

    revise

--- task/macOS/git-update.sh
+++ task/macOS/git-update.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
 uptask() {
+
+    splash
+
     git add .
     read -p "Commit message: " msg
     git commit -m $msg
     git push -u origin master
+}
+
+splash() {
+    echo "Uptool Git Utils Beta"
+    echo ""
 }
