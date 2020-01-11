9abf2024fe525e2acf12c11f4b6f88d84d77a2c2
Shota Shimazu <hornet.live.mf@gmail.com>
2020-01-11 23:34:45 +0900

    revise

--- lib/commands.sh
+++ lib/commands.sh
@@ -8,8 +8,7 @@ cliarg() {
     fi
 
     if type $1 > /dev/null 2>&1; then
-        echo $1
-        `$1`
+        eval $1
     else
         echo "This command does not exist!"
     fi
---
+++ tests/eval.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+CMD="nano eval.sh"
+
+eval $CMD
