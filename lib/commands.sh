7e25dd275760e01f0f8d3e90e6a6fdf85ddae2c3
Shota Shimazu <hornet.live.mf@gmail.com>
2020-01-11 19:36:26 +0900

    revise

--- lib/commands.sh
+++ lib/commands.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+
 cliarg() {
     if [ ${1} = "ls" ]; then
         ls -a
+    fi
+
+    if [ ${1} = "chmod" ]; then
+        if [ ${2} = "dev" ]; then
+            . ~/.setuptools/lib/masterdev.sh
+        fi
     fi
 }
