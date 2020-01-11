26ba45beac73b6b70f81a6edb368ab45400ef95b
Shota Shimazu <hornet.live.mf@gmail.com>
2020-01-11 14:06:07 +0900

    revise

--- setuptools-help.sh
+++ setuptools-help.sh
@@ -5,8 +5,18 @@
 echo
 echo "Setuptools v1.1.0"
 echo
-echo "install [package]                  Install or setup new package."
-echo "search  [package]                  Search setup strategies."
-echo "configure                          Configure setuptools."
-echo "help                               Show usage."
+echo "install     i [package]                Install or setup new package."
+echo "search      s [package]                Search setup strategies."
+echo "update      u [package]                Update package."
+echo "uninstall     [package]                Remove package."
+echo "task        t [task]                   Run uptool task."
+echo "configure   cfg                        Create configuration."
+echo "help        h                          Show help."
+echo "version     v                          Show version."
+echo
+echo "--show-user-log                        Show user history."
+echo "--show-cached-strategies               Show list of cached strategies."
+echo "--clear-strategies                     Remove & clear cached strategies."
+echo "--show-cached-tasks                    Show list of cached tasks."
+echo "--dev-master                           Launch developer tools prompt."
 echo
