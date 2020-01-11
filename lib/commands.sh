c9221b4b394f6244128a9d0ab6aff1eeccf59c44
Shota Shimazu <hornet.live.mf@gmail.com>
2020-01-11 23:46:40 +0900

    revise

--- lib/commands.sh
+++ lib/commands.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
 
+. ~/.setuptools/lib/masterdev.sh
+
+
 cliarg() {
 
     if [ ${1} = "exit" ]; then
         echo "Terminating session..."
+    fi
+
+    if [ ${1} = "back" ]; then
+        cd ..
+    fi
+
+    if [ ${1} = "signall" ]; then
+        sign_master
     fi
 
     if type $1 > /dev/null 2>&1; then
