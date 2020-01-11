04a43db543a7cc02279f549ed4e3ab8a3f036504
Shota Shimazu <hornet.live.mf@gmail.com>
2020-01-11 23:48:22 +0900

    micro shell

--- lib/commands.sh
+++ lib/commands.sh
@@ -8,14 +8,17 @@ cliarg() {
 
     if [ ${1} = "exit" ]; then
         echo "Terminating session..."
+        return
     fi
 
     if [ ${1} = "back" ]; then
         cd ..
+        return
     fi
 
     if [ ${1} = "signall" ]; then
         sign_master
+        return
     fi
 
     if type $1 > /dev/null 2>&1; then
