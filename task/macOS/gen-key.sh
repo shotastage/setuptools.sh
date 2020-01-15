8b5f96b23b21e2e3cbb2a4543fdb2a290243b1b8
Shota Shimazu <hornet.live.mf@gmail.com>
2020-01-16 00:16:45 +0900

    revise

--- task/macOS/gen-key.sh
+++ task/macOS/gen-key.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 uptask() {
-    openssl genpkey -algorithm RSA >> privkey.rsa
+    openssl genpkey -algorithm RSA \
+        -pkeyopt rsa_keygen_bits:4094 \
+        -pkeyopt rsa_keygen_pubexp:65537 | \
+        openssl pkcs8 -topk8 -nocrypt -outform der > rsa-private-key.p8
 }
