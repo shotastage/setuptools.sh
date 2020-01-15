2f373ac041144026291e96874a8033c883d69444
Shota Shimazu <hornet.live.mf@gmail.com>
2020-01-15 23:47:41 +0900

    revise

--- lib/executor.sh
+++ lib/executor.sh
@@ -58,12 +58,12 @@ exetask() {
     echo ${TASK_MASTER_URL}/${1}.sh
 
     echo "SSS"
-    echo "https://raw.githubusercontent.com/shotastage/setuptools.sh/master/task/$(operating_system)/signatures/${2}.sh.sig"
+    echo "https://raw.githubusercontent.com/shotastage/setuptools.sh/master/task/$(operating_system)/signatures/${1}.sh.sig"
 
     # Download script
     curl --silent "${TASK_MASTER_URL}/${1}.sh" -o $WORKING_DIRECTORY/tasks/${1}.sh
     chmod +x $WORKING_DIRECTORY/tasks/${1}.sh
-    verify_taskscript ${1}.sh "https://raw.githubusercontent.com/shotastage/setuptools.sh/master/task/$(operating_system)/signatures/${2}.sh.sig"
+    verify_taskscript ${1}.sh "https://raw.githubusercontent.com/shotastage/setuptools.sh/master/task/$(operating_system)/signatures/${1}.sh.sig"
 
 
     . $WORKING_DIRECTORY/tasks/${1}.sh
