baae8772c8a94dd60fb851d6f41ceb251bdaa589
Shota Shimazu <hornet.live.mf@gmail.com>
2020-01-21 23:59:30 +0900

    revise

--- strategies/macOS/pgadmin4.sh
+++ strategies/macOS/pgadmin4.sh
@@ -12,10 +12,10 @@ install() {
     fi
     echo "⬇️  pgAdminをインストールします."
     echo "⬇️  ファイルをダウンロードしています..."
-    download "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v4.16/macos/pgadmin4-4.16.dmg" pgadmin4-4.16.dmg
-    if [ -e pgadmin4-4.16.dmg ]; then
+    download "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v4.17/macos/pgadmin4-4.17.dmg" pgadmin4-4.17.dmg
+    if [ -e pgadmin4-4.17.dmg ]; then
         echo "💿  イメージをマウントしています..."
-        hdiutil mount pgadmin4-4.16.dmg >> /dev/null
+        hdiutil mount pgadmin4-4.17.dmg >> /dev/null
         echo "⬇️  アプリケーションをインストールしています..."
         cp -r /Volumes/pgAdmin\ 4/pgAdmin\ 4.app /Applications/
     else
@@ -28,7 +28,7 @@ install() {
     echo "💿  イメージをアンマウントしています..."
     umount /Volumes/pgAdmin\ 4/ >> /dev/null
     echo "🧹  クリーンアップしています..."
-    rm pgadmin4-4.16.dmg
+    rm pgadmin4-4.17.dmg
 }
 
 
