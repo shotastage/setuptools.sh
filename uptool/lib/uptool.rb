7cf47620997324f6ad4b610bc52d9e3c58783ef6
Shota Shimazu <shotastage@users.noreply.github.com>
2021-01-21 22:14:01 +0900

    remove ruby-based uptool

--- uptool/lib/uptool.rb
+++
@@ -1,7 +0,0 @@
-require "uptool/version"
-require "uptool/cli"
-
-module Uptool
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- uptool/lib/uptool/version.rb
+++
@@ -1,3 +0,0 @@
-module Uptool
-  VERSION = "0.1.0"
-end
