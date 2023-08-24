7258dbe2afea3ca2427c248aa9dd6bb8d279b027
shotastage <shota.shimazu@magicalsoft.app>
2023-08-25 00:41:06 +0900

    revise

--- lib/tasks.sh
+++ lib/tasks.sh
@@ -26,6 +26,7 @@ show_task_script() {
     fi
 
     # Download script
+    echo "Downloading ${1} ..."
     curl --silent "${TASK_MASTER_URL}/${1}.sh" -o $WORKING_DIRECTORY/tasks/${1}.sh
     chmod +x $WORKING_DIRECTORY/tasks/${1}.sh
     # verify_taskscript ${1}.sh "https://raw.githubusercontent.com/shotastage/setuptools.sh/master/task/$(operating_system)/signatures/${1}.sh.sig"
